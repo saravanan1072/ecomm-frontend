9218cefc9ff4dbc323f65b85d1faa599f573232b
saravanan1072 <saravanan10722@gmail.com>
2023-04-04 22:17:19 +0530

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,14 +11,33 @@ import { useEffect,useState } from 'react';
 import Footer from "../common/Footer";
 import Navbar from '../utilities/Navbar';
 import Carosel from '../common/Carosal';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 export default function Home() {
   const [state,setState]=useState([])
-  const [product,setProduct]=useState()
+  const [product,setProduct]=useState([]);
+  const [future,setFuture]=useState([]);
   useEffect(()=>{
     ApiCaller({url:"https://ecombackend-nr3r.onrender.com/homecaro",
     method:"get"
   })
 .then((res)=>setState(res.data))
+.catch((rej)=>{console.log(rej)})
+
+ApiCaller({url:"http://localhost:3001/futurecaro",
+method:"get"
+})
+.then((res)=>{
+setFuture(res.data)
+console.log(res.data)
+}
+)
 .catch((rej)=>{console.log(rej)})
 
 ApiCaller({url:"https://ecombackend-nr3r.onrender.com/home",
@@ -32,7 +51,32 @@ method:"get"
     dispatch(update());
 
  }
-  console.log(state)
+  const handleLoad=()=>{
+    console.log("fun");
+    
+    product?.slice(6,9).map((item) => {
+      return (
+        <div className="Hbestseller " key={item.id}>
+          <img className="Hchild-image-size" src={item.image} alt="images" />
+          <br />
+          <b className="header">
+            <b className="header-title">{item.title} </b>
+          </b>
+          <br />
+           <Rating maxRating={5} rating={item.rating} />     
+          <span className="hprice">
+            <i className="fa-solid fa-indian-rupee-sign"></i>
+            <b className="mrp-price">M.R.P :{item.discountPrice}</b>
+            </span>
+         
+          <br /> <br/>
+          <button className="add-to-cart"  onClick={addToCart}>Add to cart</button>
+        </div>
+      );
+    })
+  
+
+  }
   return (
     <>
     <Navbar/>
@@ -44,7 +88,7 @@ method:"get"
     <h2 className='best'>Best seller </h2>
     <div id='home-body'>
 
-    {product?.map((item) => {
+    {product?.slice(0,6).map((item) => {
           return (
             <div className="Hbestseller " key={item.id}>
               <img className="Hchild-image-size" src={item.image} alt="images" />
@@ -64,11 +108,14 @@ method:"get"
             </div>
           );
         })}
+
+       
   
     
     
     
     </div>
+    <button className='submit' onClick={handleLoad}>Load More</button>
 
 
     <div className='homebody'>
@@ -80,21 +127,45 @@ method:"get"
       <div className='home-footer-child'>
         <img src={shipping} alt=" shipping"/> <br/>
         <p  className='bold'>FREE SHIPPING</p>
-        <div className='text'>The Flexible Box Layout Module, makes it easier to design flexible responsive layout structure without using float or positioning.</div>
+        <div className='text'>The package has been loaded on a truck and departed for the final distribution center. That means the package could be anywhere between the origin location and the destination termina</div>
       </div>
       <div className='home-footer-child'>
         <img src={refund} alt=" refund"/> <br/>
         <p className='bold' >100% REFUND</p>
-        <div className='text'>The Flexible Box Layout Module, makes it easier to design flexible responsive layout structure without using float or positioning.</div>
+        <div className='text'>A refund is when you have charged a payer, and need to cancel the payment and return the funds to the payer.</div>
         </div>
       <div className='home-footer-child'>
         <img src={support} alt=" support"/> <br/>
         <p className='bold'>SUPPORT 24/7</p>
-        <div className='text'>The Flexible Box Layout Module, makes it easier to design flexible responsive layout structure without using float or positioning.</div>
+        <div className='text'> Giving assistance or support to customer  to provide help with something that is useful or necessary in achieving in an order.</div>
       </div>
     </div>
+    <h1>Feature products</h1>
+   <div>
 
-   <div><h1>Feature products</h1></div>
+         <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+       {future?.map((item) => {
+          return (
+            <div className="Hbestseller" key={item.id}>
+             <SwiperSlide key={item.id}> <img className="Hchild-image-size" src={item.image} alt="images" /></SwiperSlide>
+              <br />
+            </div>
+          );
+        })}
+        
+    </Swiper>
+    
+   </div>
 
   
 
